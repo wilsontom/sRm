5c482931aab1227e85c6a18e4cfbfda51fbb1f84
wilsontom <wilsontom89@gmail.com>
2016-06-28 22:12:35 +0100

    update S4 generics

--- R/allGenerics.R
+++ R/allGenerics.R
@@ -12,3 +12,31 @@ setGeneric(name = "combineTransitions",
            {standardGeneric("combineTransitions")}
 )
 
+
+#' Show meta data
+#' @rdname meta
+#'
+#' @param object a SRM object
+#' @return NULL
+#'
+#' @author Tom Wilson \email{tpw2@@aber.ac.uk}
+#' @export
+
+setGeneric(name = "meta",
+           def = function(object)
+           {standardGeneric("meta")}
+)
+
+#' Show transitions
+#' @rdname transitions
+#'
+#' @param object a SRM object
+#' @return NULL
+#'
+#' @author Tom Wilson \email{tpw2@@aber.ac.uk}
+#' @export
+
+setGeneric(name = "transitions",
+           def = function(object)
+           {standardGeneric("transitions")}
+)
