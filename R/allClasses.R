3b3a508989ac20f3444d1973489a3a50b1039bc2
wilsontom <wilsontom89@gmail.com>
2016-06-28 16:50:38 +0100

    set class definition

---
+++ R/allClasses.R
@@ -0,0 +1,24 @@
+#' SRM
+#' @rdname SRMclass
+#'
+#' A S4 class to store selective reaction monitoring (SRM) mass spectrometry (MS) data
+#'
+#' @slot env
+#' @slot SHA1
+#' @slot meta
+#' @slot totalIonCount
+#' @slot index
+#' @slot peaks
+#' @slot header
+#'
+setClass(Class = "SRM", representation = representation(
+          env = "environment",
+          SHA1 = "character",
+          meta = "list",
+          totalIonCount = "data.frame",
+          index = "vector",
+          peaks = "list",
+          header = "data.frame"
+        )
+    )
+
