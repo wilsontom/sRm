b38b9b759e01a5223985d95da1eb5ff6dcf27068
wilsontom <wilsontom89@gmail.com>
2016-06-28 20:03:04 +0100

    add some tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(sRm)
+
+test_check("sRm")
---
+++ tests/testthat/test-sRm.R
@@ -0,0 +1,15 @@
+context("sRm")
+
+test_that("sRm-works", {
+  example_file <- system.file("data/example_file.mzML", package = "sRm")
+
+  srm_peaks <- openSRMfile(example_file)
+  expect_true(isS4(srm_peaks))
+  expect_that(nrow(srm_peaks@header), equals(length(srm_peaks@peaks)))
+  expect_that(nrow(srm_peaks@header), equals(length(srm_peaks@index)))
+
+  srm_comb <- combineTransitions(srm_peaks)
+  expect_true(is.list(srm_comb))
+  expect_that(length(srm_comb), equals(length(unique(srm_peaks@header$parentMz))))
+
+})
