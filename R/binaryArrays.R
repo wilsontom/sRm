a61255dc5c897a0e4136399e423611f99b6c2152
wilsontom <wilsontom89@gmail.com>
2016-07-22 14:57:47 +0100

    return precision for each array

--- R/binaryArrays.R
+++ R/binaryArrays.R
@@ -3,9 +3,10 @@
 #' Extract all the \code{binaryDataArrays} in the specified \code{mzML} file.
 #'
 #' @param xmlDoc a \code{xml} document
-#' @return a list of two elements; array name and array value. Array name is character vector specifying
+#' @return a list of three elements; array name, array value and array precision. Array name is character vector specifying
 #' the type of array (time array or intensity). Array value is a large character vector where each element
-#' is the \code{base64} encdoded value of the \code{binaryDataArray}
+#' is the \code{base64} encdoded value of the \code{binaryDataArray}. Arrray precision is the value specifing the encoded precision
+#' of each array (32-bit or 64-bit)
 #'
 #' @author Tom Wilson \email{tpw2@@aber.ac.uk}
 #' @keywords internal
@@ -28,9 +29,17 @@ binaryArrays <- function(xmlDoc)
 
   array_type <- lapply(arrays, function(x)(xml_attrs(xml_children(x)[[3]])[["name"]]))
 
+  array_prec <- lapply(arrays, function(x)(xml_attrs(xml_children(x)[[1]])[["name"]]))
+
   array_binary_value <- lapply(arrays, function(x)(xml_text(xml_children(x)[[4]])))
 
-  array_res <- list(name = unlist(array_type), value = unlist(array_binary_value))
+  array_res <- list(name = unlist(array_type), value = unlist(array_binary_value), precision = unlist(array_prec))
+
+  ## clean up precisin
+
+  array_res$precision <- gsub("64-bit float", 64, array_res$precision)
+  array_res$precision <- gsub("32-bit float", 32, array_res$precision)
+  array_res$precision <- as.numeric(array_res$precision)
 
   return(array_res)
   }
