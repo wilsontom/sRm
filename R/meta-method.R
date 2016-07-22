79b12c9959ae626fa7d6222adbabe08ba282768a
wilsontom <wilsontom89@gmail.com>
2016-07-22 14:59:26 +0100

    update due to bug fix

--- R/meta-method.R
+++ R/meta-method.R
@@ -8,11 +8,19 @@ setMethod("meta", signature = "SRM",
           cat("\n", "\n")
           cat("Instrument", object@meta$instrument_model, sep = " : ")
           cat("\n", "\n")
-          cat("Precision", object@meta$precision, sep = " : ")
+          if(length(object@meta$precision) == 1){cat("Precision", object@meta$precision, sep = " : ")}
+
+          if(length(object@meta$precision > 1)){
+            cat("Precision:", "\n")
+            for(i in 1:length(object@meta$precision)){
+              cat(object@meta$precision[i], sep = " : ")
+              cat("\n")
+            }
+          }
           cat("\n", "\n")
           cat("Compression", object@meta$compressin, sep = " : ")
           cat("\n", "\n")
-          cat("Conversion scheme", object@meta$schema, sep = " : ")
+          cat("Conversion schema", object@meta$schema, sep = " : ")
           cat("\n", "\n")
        }
 )
