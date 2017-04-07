3636315de8e85009461f72e802aa7e1758b760a7
wilsontom <wilsontom89@gmail.com>
2017-04-07 17:10:36 +0100

    fix typo

--- R/allClasses.R
+++ R/allClasses.R
@@ -5,7 +5,7 @@
 #' @slot SHA1 character vector of the original SHA-1 checksum value
 #' @slot meta list containing file meta data
 #' @slot totalIonCount \code{data.frame} of sample total ion count (TIC)
-#' @slot filter character vector of scan filters; taken directly from \code{chromatogram idRef's}
+#' @slot filter character vector of scan filters; taken directly from \code{chromatogram idRefs}
 #' @slot index character vector of readable scan index's; ie \code{SRM SIC 341.014,114.098}
 #' @slot peaks list of peak data
 #' @slot header \code{data.frame} of summary information for each scan index; \code{parentMz, Q3mz, totalIonCount, basePeakInt}
