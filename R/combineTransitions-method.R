6b6f323ed3f543ab6ecdb472009adcacb5f14952
wilsontom <wilsontom89@gmail.com>
2017-04-11 16:14:20 +0100

    bit of a hack to try and catch dynamic MRM methods

--- R/combineTransitions-method.R
+++ R/combineTransitions-method.R
@@ -8,9 +8,15 @@
 setMethod(f = combineTransitions, signature = "SRM",
           function(object){
 
-          trans_sets <- split(object@peaks, object@header$parent)
+          if(length(grep("Q1",object@filter))!= 0){
+            unique_scan_filter <- unique_idrefs(object)
+          }else{
+            unique_scan_filter <- object@header$parent
+          }
 
-          trans_header <- split(object@header, object@header$parent)
+          trans_sets <- split(object@peaks, unique_scan_filter)
+
+          trans_header <- split(object@header, unique_scan_filter)
 
           trans_dim <- NULL
           for(i in seq_along(trans_sets)){
@@ -21,33 +27,56 @@ setMethod(f = combineTransitions, signature = "SRM",
           idx <- which(equality_test != 1)
 
           if(length(idx != 0)){
-            stop("different Rt lengths detected in transition sets; unable to combine", call. = FALSE)
+            message("WARNING: The follwing transitions have been removed from the 'transition' object:")
+            cat("\n")
+            for (i in seq_along(idx)){
+              idx_n <- as.numeric(rownames(trans_header[idx][[i]]))
+                for(k in seq_along(idx_n)){
+                  message(object@filter[idx_n[k]], "\n")
+                }
+              cat("\n")
+            }
+          }
+
+          if(length(idx != 0)){
+            trans_sets <- trans_sets[-idx]
+            trans_header <- trans_header[-idx]
           }
 
           trans_dfs <- lapply(trans_sets, function(x)(do.call("cbind",x)))
 
           int_idx <- rt_idx <- NULL
-          for(i in 1:length(trans_dfs)){
+          for(i in seq_along(trans_dfs)){
             int_idx[[i]] <- grep("int", names(trans_dfs[[i]]))
             rt_idx[[i]] <- grep("rt", names(trans_dfs[[i]]))
           }
 
           int_sum <- NULL
-          for(i in 1:length(trans_dfs)){
-            int_sum[[i]] <- apply(trans_dfs[[i]][,int_idx[[i]]],1,sum)
+          for(i in seq_along(trans_dfs)){
+            if(length(int_idx[[i]]) == 1){
+              int_sum[[i]] <- trans_dfs[[i]][,int_idx[[i]]]
+            }else{
+              int_sum[[i]] <- apply(trans_dfs[[i]][,int_idx[[i]]],1,sum)
+            }
           }
 
           trans_df_comb <- list()
-          for(i in 1:length(int_sum)){
+          for(i in seq_along(int_sum)){
             trans_df_comb[[i]] <- data.frame(rt = trans_dfs[[i]][,rt_idx[[i]]][[1]],int = trans_dfs[[i]][,int_idx[[i]]], TIC = int_sum[[i]])
-            names(trans_df_comb[[i]]) <- gsub(".int", "", names(trans_df_comb[[i]]))
-            patsub <- paste0(".",unique(trans_header[[i]]$parentMz))
-            names(trans_df_comb[[i]]) <- gsub(patsub,"", names(trans_df_comb[[i]]))
-            names(trans_df_comb[[i]]) <- gsub("int.", "int-", names(trans_df_comb[[i]]))
+
+            nmidx <- which(names(trans_df_comb[[i]]) != "rt" | names(trans_df_comb[[i]]) != "TIC")
+
+            names(trans_df_comb[[i]])[-nmidx] <- "int"
+
+
+            #names(trans_df_comb[[i]]) <- gsub(".int", "", names(trans_df_comb[[i]]))
+            #patsub <- paste0(".",unique(trans_header[[i]]$parent))
+            #names(trans_df_comb[[i]]) <- gsub(patsub,"", names(trans_df_comb[[i]]))
+            #names(trans_df_comb[[i]]) <- gsub("int.", "int-", names(trans_df_comb[[i]]))
           }
 
           new_names <- NULL
-          for(i in 1:length(trans_header)){
+          for(i in seq_along(trans_header)){
             pMz <- paste("Q1", trans_header[[i]][1,"parent"], sep = ": ")
             qMzs <- paste(trans_header[[i]][,"product"], collapse = "//")
             qMz2 <- paste("Q3", qMzs, sep = ": ")
