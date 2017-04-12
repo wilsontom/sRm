b51e96d89e103dc8000d64f6a864ce918eca4ac2
wilsontom <wilsontom89@gmail.com>
2017-04-12 14:10:19 +0100

    update rox info based on new object@header

--- R/allClasses.R
+++ R/allClasses.R
@@ -6,9 +6,9 @@
 #' @slot meta list containing file meta data
 #' @slot totIonCount \code{data.frame} of sample total ion count (TIC)
 #' @slot filter character vector of scan filters; taken directly from \code{chromatogram idRefs}
-#' @slot index character vector of readable scan index's; ie \code{SRM SIC 341.014,114.098}
+#' @slot index character vector of readable scan index's; ie \code{Q1: 153.01 --> Q3: 65.271 (-)}
 #' @slot peaks list of peak data
-#' @slot header \code{data.frame} of summary information for each scan index; \code{parentMz, Q3mz, totalIonCount, basePeakInt}
+#' @slot header \code{data.frame} of summary information for each scan index; \code{parent, product, polarity, totIonCount, basePeakInt}
 #'
 setClass(Class = "SRM", representation = representation(
           SHA1 = "character",
