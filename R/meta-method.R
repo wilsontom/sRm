e1b98a0a5334ee8b9fd4667d05dedad6e4467a61
wilsontom <wilsontom89@gmail.com>
2017-10-10 16:29:59 +0100

    remove redundant

--- R/meta-method.R
+++ R/meta-method.R
@@ -4,27 +4,11 @@
 setMethod("meta", signature = "SRM",
           function(object){
           cat("\n")
-          cat("File ID : ", object@meta$fileID)
-          cat("\n", "\n")
-          cat("Instrument : ", object@meta$instrument)
+          cat("File ID : ", object@meta$file_id)
           cat("\n", "\n")
           cat("Instrument Model : ", object@meta$instrument_model)
           cat("\n", "\n")
-          cat("Instrument Serial Number : ", object@meta$instrument_serial)
-          cat("\n", "\n")
-          if(length(object@meta$precision) == 1){cat("Precision", object@meta$precision, sep = " : ")}
-
-          if(length(object@meta$precision > 1)){
-            cat("Precision:", "\n")
-            for(i in 1:length(object@meta$precision)){
-              cat(object@meta$precision[i], sep = " : ")
-              cat("\n")
-            }
-          }
-          cat("\n")
-          cat("Compression", object@meta$compressin, sep = " : ")
-          cat("\n", "\n")
-          cat("Conversion Schema", object@meta$schema, sep = " : ")
+          cat("Conversion Schema", object@meta$mzml_schema, sep = " : ")
           cat("\n", "\n")
        }
 )
