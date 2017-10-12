41b584fb05da2ec336277b8bd25ed9528b2049d0
wilsontom <wilsontom89@gmail.com>
2017-10-12 12:07:11 +0100

    re-format and bug fix

--- R/plotMulti-method.R
+++ R/plotMulti-method.R
@@ -2,53 +2,98 @@
 #' @importFrom ggplot2 guides theme_classic
 
 setMethod("plotMulti", signature = "SRM",
-          function(object,idn, labels = NULL){
+          function(object,
+                   idn,
+                   addLabels = TRUE,
+                   labels = NULL) {
+            idn_names <- object@index[idn]
+            idn_peaks <- object@peaks[idn]
 
-          idn_names <- object@index[idn]
-          idn_peaks <- object@peaks[idn]
+            for (i in seq_along(idn_peaks)) {
+              if (is.null(labels)) {
+                idn_peaks[[i]] <-
+                  tibble::add_column(idn_peaks[[i]], name = rep(idn_names[[i]], nrow(idn_peaks[[i]])))
+              }
+              if (!is.null(labels)) {
+                idn_peaks[[i]] <-
+                  tibble::add_column(idn_peaks[[i]], name = rep(labels[i], nrow(idn_peaks[[i]])))
+              }
 
-          idn_list <- lapply(idn_peaks, function(x)(idn_list <- data.frame(rt = x$rt, int = x$int)))
+            }
 
-          for(i in seq_along(idn_list)){
-            nm_tmp <- rep(names(idn_list)[i], nrow(idn_list[[i]]))
-            idn_list[[i]] <- data.frame(idn_list[[i]], name = nm_tmp)
-          }
+            peaks_all <- data.frame(do.call('rbind', idn_peaks))
 
-          idn_df <- do.call("rbind", idn_list)
-          idn_df[,"name"] <- as.character(idn_df[,"name"])
-          idn_df[,"name"] <- gsub("SRM SIC ", "", idn_df[,"name"])
+            peaks_all[, 'name'] <-
+              factor(peaks_all[, 'name'], levels = unique(peaks_all[, 'name']))
+
+            if (addLabels == FALSE) {
+              plot_multi <-
+                ggplot(data = peaks_all,
+                       aes_string(
+                         x = 'rt',
+                         y = 'int',
+                         group = 'name',
+                         colour = 'name'
+                       )) +
+                geom_line() + theme_bw() +
+                guides(group = "none") +
+                guides(colour = "none")  +
+                xlab("Retention Time (mins)") +
+                ylab("Intensity") +
+                theme(
+                  axis.text.y = element_text(size = 11, face = "bold"),
+                  axis.text.x = element_text(size = 11, face = "bold"),
+                  axis.title.y = element_text(size = 11, face = "bold"),
+                  axis.title.x = element_text(size = 11, face = "bold")
+                )
+              plot(plot_multi)
+            }
 
 
-          label_list <- lapply(idn_list, function(x)(which(x$int == max(x$int))))
-          label_df <- data.frame(matrix(nrow = length(label_list), ncol = 3))
-
-          for(i in seq_along(label_list)){
-            label_df[i,1] <- idn_list[[i]][label_list[[i]],"rt"]
-            label_df[i,2] <- idn_list[[i]][label_list[[i]],"int"]
-            label_df[i,3] <- names(label_list)[[i]]
-          }
-
-          names(label_df) <- c("rt", "int", "name")
-          label_df[,"name"] <- gsub("SRM SIC ", "", label_df[,"name"])
+            if (addLabels == TRUE) {
+              label_df <-
+                data.frame(matrix(ncol = 3, nrow = length(idn)))
+              names(label_df) <- c('rt', 'int', 'name')
 
 
+              for (i in seq_along(idn_peaks)) {
+                maxint <-
+                  which(idn_peaks[[i]][, 'int'] == max(idn_peaks[[i]][, 'int']))
+                label_df[i, 'rt'] <- idn_peaks[[i]][maxint, 'rt']
+                label_df[i, 'int'] <- idn_peaks[[i]][maxint, 'int']
 
-          if(!is.null(labels)){
-            midx <- match(idn_df$name, label_df$name)
-            idn_df$name <- labels[midx]
-            label_df[,"name"] <- labels
-          }
+                if (is.null(labels)) {
+                  label_df[i, 'name'] <- unique(idn_peaks[[i]][, 'name'])
 
-          plot_multi <- ggplot(data = idn_df, aes_string(x = 'rt', y = 'int', group = 'name', colour = 'name')) +
-                            geom_line() + theme_classic() + ggrepel::geom_label_repel(data =label_df, aes_string(x = 'rt', y = 'int', label = 'name')) +
-                            guides(group = "none") +
-                            guides(colour = "none")  +
-                            xlab("Retention Time (mins)") +
-                            ylab("Intensity") +
-                      theme(axis.text.y = element_text(size = 12, face = "bold"),
-                            axis.text.x = element_text(size = 12, face = "bold"),
-                            axis.title.y = element_text(size = 12, face = "bold"),
-                            axis.title.x = element_text(size = 12, face = "bold"))
-          print(plot_multi)
-          }
-)
+                } else{
+                  label_df[i, 'name'] <- labels[i]
+
+                }
+
+              }
+              label_df[, 'name'] <- factor(label_df[, 'name'])
+
+              plot_multi <-
+                ggplot(data = peaks_all,
+                       aes_string(
+                         x = 'rt',
+                         y = 'int',
+                         group = 'name',
+                         colour = 'name'
+                       )) +
+                geom_line() + theme_bw()  + ggrepel::geom_label_repel(data = label_df, aes_string(x = 'rt', y = 'int', label = 'name')) +
+                guides(group = "none") +
+                guides(colour = "none")  +
+                xlab("Retention Time (mins)") +
+                ylab("Intensity") +
+                theme(
+                  axis.text.y = element_text(size = 11, face = "bold"),
+                  axis.text.x = element_text(size = 11, face = "bold"),
+                  axis.title.y = element_text(size = 11, face = "bold"),
+                  axis.title.x = element_text(size = 11, face = "bold")
+                )
+
+              plot(plot_multi)
+            }
+
+          })
