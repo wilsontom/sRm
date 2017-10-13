0a2d608fefe7297a921cc323fee2cad09dbc6e89
wilsontom <wilsontom89@gmail.com>
2017-10-13 13:23:53 +0100

    minor changes to unit tests

--- tests/testthat/test-sRm.R
+++ tests/testthat/test-sRm.R
@@ -9,7 +9,7 @@ test_that("sRm-works", {
   expect_that(nrow(srm_peaks@header), equals(length(srm_peaks@index)))
 
   srm_comb <- combineTransitions(srm_peaks)
-  expect_true(is.list(srm_comb))
+  expect_true(inherits(srm_comb, 'transition'))
   expect_that(length(srm_comb), equals(length(unique(srm_peaks@header$parent))))
 
   expect_true(is.null(transitions(srm_peaks)))
