c22f22e862bedfd8a3cbc77faf04372267cccd65
wilsontom <wilsontom89@gmail.com>
2019-05-17 12:56:33 +0100

    plot all transitions which have the same parent (Q1) ion

---
+++ R/plotParent-method.R
@@ -0,0 +1,46 @@
+#' @rdname plotParent
+#'
+
+setMethod('plotParent', signature = 'SRM',
+          function(object, parentMass) {
+            plot_tr_name <-
+              object@header %>% filter(Q1 == !!parentMass) %>% select(index) %>% distinct()
+
+            plot_tibble <-
+              object@rawChrom %>% filter(index %in% plot_tr_name$index)
+
+            transition_match <-
+              match(plot_tibble$index, object@header$index)
+
+            plot_tibble$index <-
+              object@header$transition[transition_match]
+
+
+
+            plot_out <- ggplot(data = plot_tibble,
+                               aes_string(
+                                 x = 'rt',
+                                 y = 'int',
+                                 group = 'index',
+                                 colour = 'index'
+                               )) + geom_line(size = 0.50) + theme_bw() +
+              theme(legend.position = 'top') +
+              theme(legend.title = element_blank()) +
+              theme(strip.text.x = element_text(size = 10)) +
+              theme(
+                axis.text.y = element_text(size = 10),
+                axis.text.x = element_text(size = 10),
+                axis.title.y = element_text(size = 10),
+                axis.title.x = element_text(size = 10)
+              ) +
+              scale_x_continuous(breaks = seq(
+                from  = 0,
+                to = round(max(plot_tibble$rt), digits = 1),
+                by = 2
+              )) +
+              xlab("Retention Time (mins)") + ylab("Intensity") +
+              facet_wrap(~ sampleID)
+
+            return(plot_out)
+
+          })
