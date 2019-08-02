e691665a2d7c608688e4438a56e463ce972857ae
wilsontom <wilsontom89@gmail.com>
2019-08-02 10:00:54 +0100

    start work on peak smoothing methods

---
+++ R/smoothChrom-method.R
@@ -0,0 +1,27 @@
+#' @rdname smoothChrom
+#'
+
+setMethod('smoothChrom', signature = 'SRM',
+          function(object, method, ...) {
+
+          arguments <- list(...)
+
+          chrom_split  <-
+            object@rawChrom %>% group_by(sampleID, index) %>% group_split()
+
+          if(method == 'sgolay') {
+            smoothed_chrom <- chrom_split %>% purrr::map(., ~ {
+              savitzky_golay_opt(.$rt, .$int, ford = arguments$ford)
+            })
+          }
+
+          for(i in seq_along(chrom_split)){
+            chrom_split[[i]]$int <- smoothed_chrom[[i]]
+          }
+
+          object@transformedChrom <- chrom_split %>% bind_rows()
+
+          return(object)
+
+          }
+)
