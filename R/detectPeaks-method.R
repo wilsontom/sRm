87497ee92df1ed36662010ca0d3426a3d216f3b9
wilsontom <wilsontom89@gmail.com>
2019-08-09 17:04:37 +0100

    method for peak detection

---
+++ R/detectPeaks-method.R
@@ -0,0 +1,51 @@
+#' @rdname detectPeaks
+#'
+
+setMethod('detectPeaks', signature = 'SRM',
+          function(object, method, ...){
+
+            arguments <- list(...)
+
+
+            if(length(object@transformedChrom) == 0){
+              chrom_split  <-
+                object@rawChrom %>% group_by(sampleID, index) %>% group_split()
+            }else{
+              chrom_split  <-
+                object@transformedChrom %>% group_by(sampleID, index) %>% group_split()
+            }
+
+
+
+            if(method == 'pracma'){
+
+              chromPeaks <-
+                purrr::map(chrom_split, ~ {
+                  pracma_peak_picking(.$rt, .$int)
+                })
+
+              for(i in seq_along(chromPeaks)) {
+                chromPeaks[[i]] <-
+                  chromPeaks[[i]] %>% mutate(sampleID = chrom_split[[i]]$sampleID[1],
+                                             index = chrom_split[[i]]$index[1]) %>%
+                  select(sampleID, index, rt, rtmin, rtmax, int, area, peakId)
+
+              }
+
+
+              object@peaks <-  chromPeaks %>% bind_rows()
+
+                }
+
+            return(object)
+
+            }
+
+
+
+
+
+
+
+
+          }
