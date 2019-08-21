7be433f9044856c6ac14fcab7a5f17c9684e12c1
wilsontom <wilsontom89@gmail.com>
2019-08-21 13:11:25 +0100

    wrapper function for xcms::peaksWithMatchedFilter

---
+++ R/matchedFilter.R
@@ -0,0 +1,38 @@
+#' Wraper for xcms matchedFilter peak detection
+#'
+#'
+#' @param rt a numeric vector of chromatogram retention time in minutes
+#' @param int a numeric vector of chromatogram intensity
+#' @param snthresh a numeric value for the Signal-to-Noise cut off to use during peak detection
+#' @return a `tibble` of detected peaks
+#'
+#' @export
+
+matchedFilter <- function(rt, int, snthresh)
+{
+  MFpeaks <-
+    xcms::peaksWithMatchedFilter(int, rt * 60, snthresh = snthresh) %>% tibble::as_tibble()
+
+  if (nrow(MFpeaks) > 0) {
+    peakTable <-
+      MFpeaks %>% dplyr::select(rt, rtmin, rtmax, maxo, into, sn) %>% mutate(peakId = seq(from = 1, to = nrow(.))) %>%
+      mutate(rt = rt / 60,
+             rtmin = rtmin / 60,
+             rtmax = rtmax / 60) %>%
+      dplyr::rename(int = maxo, area = into)
+
+
+  }
+
+  if (nrow(MFpeaks) == 0) {
+    peakTable <-
+      MFpeaks %>% dplyr::select(rt, rtmin, rtmax, maxo, into, sn) %>% mutate(peakId = 0) %>%
+      dplyr::rename(int = maxo, area = into)
+
+    peakTable[1, ] <- 0
+
+  }
+
+  return(peakTable)
+
+}
