7d7ca64ab825d4f34d33f6d7d1d1428fb3392e2e
wilsontom <wilsontom89@gmail.com>
2019-08-21 15:14:09 +0100

    import xml2

--- R/get_meta.R
+++ R/get_meta.R
@@ -16,17 +16,18 @@
 
 get_meta <- function(x)
 {
-  xmltmp <- read_xml(x)
+  xmltmp <- xml2::read_xml(x)
 
-  refGroup <- xml_find_all(xmltmp, '//d1:referenceableParamGroup')
+  refGroup <-
+    xml2::xml_find_all(xmltmp, '//d1:referenceableParamGroup')
   mzml_schema <-
-    xml_attrs(xml_children(xmltmp)[[1]])[['schemaLocation']]
+    xml2::xml_attrs(xml2::xml_children(xmltmp)[[1]])[['schemaLocation']]
 
-  UserParam <- xml_find_all(xmltmp, "//d1:userParam")
-  inst_model <- xml_attrs(UserParam)[[1]][['value']]
+  UserParam <- xml2::xml_find_all(xmltmp, "//d1:userParam")
+  inst_model <- xml2::xml_attrs(UserParam)[[1]][['value']]
 
-  runHeader <- xml_find_all(xmltmp, "//d1:run")
-  acqStamp <- xml_attrs(runHeader)[[1]][["startTimeStamp"]]
+  runHeader <- xml2::xml_find_all(xmltmp, "//d1:run")
+  acqStamp <- xml2::xml_attrs(runHeader)[[1]][["startTimeStamp"]]
   acqDate <- strsplit(acqStamp, "T")[[1]][1]
 
   acqTime <- strsplit(acqStamp, "T")[[1]][2]
@@ -34,14 +35,14 @@ get_meta <- function(x)
   acqTime_hhmm <-
     paste0(acqTime_sp[[1]][1], ":", acqTime_sp[[1]][2])
 
-  fileInfo <- xml_find_all(xmltmp, "//d1:sourceFileList")
+  fileInfo <- xml2::xml_find_all(xmltmp, "//d1:sourceFileList")
 
-  fileName <- xml_attrs(xml_children(fileInfo))[[1]][['name']]
-  fileID <- xml_attrs(xml_children(fileInfo))[[1]][['id']]
+  fileName <- xml2::xml_attrs(xml_children(fileInfo))[[1]][['name']]
+  fileID <- xml2::xml_attrs(xml_children(fileInfo))[[1]][['id']]
 
 
   meta_tibble <-
-    tibble(
+    tibble::tibble(
       name = c('Schema', 'Datestamp', 'Timestamp', 'Instrument', 'sampleID'),
       value = c(mzml_schema, acqDate, acqTime_hhmm, inst_model, fileName)
     )
