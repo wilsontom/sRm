7afe7d841fe98e261e0f8f9982f43e20d73e486d
wilsontom <wilsontom89@gmail.com>
2019-08-21 13:11:41 +0100

    support using xcms matched filter

--- R/detectPeaks-method.R
+++ R/detectPeaks-method.R
@@ -2,29 +2,27 @@
 #'
 
 setMethod('detectPeaks', signature = 'SRM',
-          function(object, method, ...){
-
+          function(object, method, ...) {
             arguments <- list(...)
 
 
-            if(length(object@transformedChrom) == 0){
+            if (length(object@transformedChrom) == 0) {
               chrom_split  <-
                 object@rawChrom %>% group_by(sampleID, index) %>% group_split()
-            }else{
+            } else{
               chrom_split  <-
                 object@transformedChrom %>% group_by(sampleID, index) %>% group_split()
             }
 
 
 
-            if(method == 'pracma'){
-
+            if (method == 'pracma') {
               chromPeaks <-
                 purrr::map(chrom_split, ~ {
                   pracma_peak_picking(.$rt, .$int)
                 })
 
-              for(i in seq_along(chromPeaks)) {
+              for (i in seq_along(chromPeaks)) {
                 chromPeaks[[i]] <-
                   chromPeaks[[i]] %>% mutate(sampleID = chrom_split[[i]]$sampleID[1],
                                              index = chrom_split[[i]]$index[1]) %>%
@@ -33,19 +31,31 @@ setMethod('detectPeaks', signature = 'SRM',
               }
 
 
-              object@peaks <-  chromPeaks %>% bind_rows()
+
+              if (method == 'matchedFilter') {
+                chromPeaks <-
+                  purrr::map(chrom_split,  ~ {
+                    matchedFilter(.$int, .$rt, snthresh = arguments$snthresh)
+                  })
+
+
+                for (i in seq_along(chromPeaks)) {
+                  chromPeaks[[i]] <-
+                    chromPeaks[[i]] %>% mutate(sampleID = chrom_split[[i]]$sampleID[1],
+                                               index = chrom_split[[i]]$index[1]) %>%
+                    select(sampleID, index, rt, rtmin, rtmax, int, area, sn, peakId)
 
                 }
 
-            return(object)
+
+              }
+
+
+
+              object@peaks <-  chromPeaks %>% bind_rows()
 
             }
 
+            return(object)
 
-
-
-
-
-
-
-          }
+          })
