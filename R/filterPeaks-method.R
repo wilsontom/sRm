dd2436a288349ae13e628707e13a7530f102b35e
wilsontom <wilsontom89@gmail.com>
2019-08-27 17:23:23 +0100

    filter peaks out based on apex position

--- R/filterPeaks-method.R
+++ R/filterPeaks-method.R
@@ -16,10 +16,34 @@ setMethod('filterPeaks', signature = 'SRM',
             peak_widths <-
               object@peaks %>% dplyr::mutate(width = (rtmax - rtmin) * 60)
 
+            # make this a method input option
             five_points_width <- 5 * meanScanTime
 
             object@peaks <-
-              object@peaks %>% dplyr::filter(width < five_points_width)
+              peak_widths %>% dplyr::filter(width > five_points_width)
+
+            # this needs speading up - add as extra method in peakDetection
+            rtApex <- list()
+            for (i in 1:nrow(object@peaks)) {
+              rawChrom <-
+                object@rawChrom %>% dplyr::filter(
+                  sampleID == object@peaks$sampleID[i] &
+                    index == object@peaks$index[i] &
+                    rt >= object@peaks$rtmin[i] &
+                    rt <= object@peaks$rtmax[i]
+                )
+
+
+              rtApex[[i]] <-
+                rawChrom$rt[rawChrom$int == max(rawChrom$int)]
+            }
+            object@peaks <-
+              object@peaks %>% dplyr::mutate(rtApex = unlist(rtApex))
+
+
+            object@peaks <-
+              object@peaks %>% dplyr::filter(rtApex != rtmin & rtApex != rtmax)
+
 
             return(object)
 
