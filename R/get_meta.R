dce5c04103f746f68e544f6243ee5b5b1a0003a7
wilsontom <wilsontom89@gmail.com>
2020-08-05 14:15:51 +0100

    if statemwnt for lcd support

--- R/get_meta.R
+++ R/get_meta.R
@@ -3,6 +3,7 @@
 #' Extract a minimal amount meta data from a `.mzML` file
 #'
 #' @param x a valid `.mzML` file
+#' @param type a character string of the original file format (`raw` or `lcd`)
 #' @return a `tibble` containing;
 #'  * mzML Schema
 #'  * Acquisition Date
@@ -12,7 +13,7 @@
 #'
 #' @export
 
-get_meta <- function(x)
+get_meta <- function(x, type = 'raw')
 {
   xmltmp <- xml2::read_xml(x)
 
@@ -21,29 +22,63 @@ get_meta <- function(x)
   mzml_schema <-
     xml2::xml_attrs(xml2::xml_children(xmltmp)[[1]])[['schemaLocation']]
 
-  UserParam <- xml2::xml_find_all(xmltmp, "//d1:userParam")
-  inst_model <- xml2::xml_attrs(UserParam)[[1]][['value']]
+  if (type == 'raw') {
+    UserParam <- xml2::xml_find_all(xmltmp, "//d1:userParam")
+    inst_model <- xml2::xml_attrs(UserParam)[[1]][['value']]
+    runHeader <- xml2::xml_find_all(xmltmp, "//d1:run")
+    acqStamp <- xml2::xml_attrs(runHeader)[[1]][["startTimeStamp"]]
+    acqDate <- strsplit(acqStamp, "T")[[1]][1]
+    acqTime <- strsplit(acqStamp, "T")[[1]][2]
+    acqTime_sp <- strsplit(acqTime, ":")
+    acqTime_hhmm <-
+      paste0(acqTime_sp[[1]][1], ":", acqTime_sp[[1]][2])
 
-  runHeader <- xml2::xml_find_all(xmltmp, "//d1:run")
-  acqStamp <- xml2::xml_attrs(runHeader)[[1]][["startTimeStamp"]]
-  acqDate <- strsplit(acqStamp, "T")[[1]][1]
+  }
 
-  acqTime <- strsplit(acqStamp, "T")[[1]][2]
-  acqTime_sp <- strsplit(acqTime, ":")
-  acqTime_hhmm <-
-    paste0(acqTime_sp[[1]][1], ":", acqTime_sp[[1]][2])
+  if (type == 'lcd') {
+    UserParam <-
+      xml2::xml_find_all(xmltmp, "//d1:instrumentConfiguration")
+    inst_model <-
+      xml2::xml_attrs(xml2::xml_children(UserParam)[[1]])[['name']]
+  }
+
 
   fileInfo <- xml2::xml_find_all(xmltmp, "//d1:sourceFileList")
 
-  fileName <- xml2::xml_attrs(xml2::xml_children(fileInfo))[[1]][['name']]
-  fileID <- xml2::xml_attrs(xml2::xml_children(fileInfo))[[1]][['id']]
+  fileName <-
+    xml2::xml_attrs(xml2::xml_children(fileInfo))[[1]][['name']]
+  fileID <-
+    xml2::xml_attrs(xml2::xml_children(fileInfo))[[1]][['id']]
 
 
-  meta_tibble <-
-    tibble::tibble(
-      name = c('Schema', 'Datestamp', 'Timestamp', 'Instrument', 'sampleID'),
-      value = c(mzml_schema, acqDate, acqTime_hhmm, inst_model, fileName)
-    )
+  if (type == 'raw') {
+    meta_tibble <-
+      tibble::tibble(
+        name = c(
+          'Schema',
+          'Datestamp',
+          'Timestamp',
+          'Instrument',
+          'sampleID'
+        ),
+        value = c(mzml_schema, acqDate, acqTime_hhmm, inst_model, fileName)
+      )
+  }
+
+  if (type == 'lcd') {
+    meta_tibble <-
+      tibble::tibble(
+        name = c(
+          'Schema',
+          'Datestamp',
+          'Timestamp',
+          'Instrument',
+          'sampleID'
+        ),
+        value = c(mzml_schema, NA, NA, inst_model, fileName)
+      )
+  }
+
 
   return(meta_tibble)
 
