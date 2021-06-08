a5655c9567ddc285d63705305b6ea53d29dce5dc
wilsontom <wilsontom89@gmail.com>
2021-06-08 21:59:12 +0100

    add argument for returning multiple Rts when there are two targets in the same transition filter

--- R/indexRT-method.R
+++ R/indexRT-method.R
@@ -4,12 +4,13 @@
 #' @param object a SRM object
 #' @param index a numeric value for the transition index
 #' @param sampleName a character string
+#' @param n a numeric value for the number of Rts to return (if > 1 is available)
 #' @return a numeric vector or Rt targets
 #'
 #' @export
 
 setMethod('indexRT', signature = 'SRM',
-          function(object, index, sampleName) {
+          function(object, index, sampleName, n = 1) {
             plot_tr_name <-
               object@transitions %>% dplyr::filter(index == !!index)
 
@@ -18,16 +19,15 @@ setMethod('indexRT', signature = 'SRM',
                                                 sampleID == !!sampleName) %>% dplyr::select(-filter)
 
             local_max_rt <-
-              round(chrom_tibble$rt[MsCoreUtils::localMaxima(chrom_tibble$int, hws = 40)], digits = 2)
+              round(chrom_tibble$rt[MsCoreUtils::localMaxima(chrom_tibble$int, hws = 10)], digits = 2)
 
             local_max_int <-
-              round(chrom_tibble$int[MsCoreUtils::localMaxima(chrom_tibble$int, hws = 40)], digits = 2) /
+              round(chrom_tibble$int[MsCoreUtils::localMaxima(chrom_tibble$int, hws = 10)], digits = 2) /
               sum(chrom_tibble$int) * 100
 
             local_max_tib <- tibble::tibble(rt = local_max_rt, int = local_max_int) %>%
-              dplyr::filter(int >= 5.0)
+              dplyr::filter(int >= 1.0) %>% dplyr::arrange(-int)
 
-              return(local_max_tib$rt)
-
+              return(local_max_tib$rt[1:n])
 
           })
